34b516344a15c1ac127a3000385853ac31ea9863
x95102003 <x95102003@gmail.com>
2017-01-02 22:53:19 +0800

    Solve it with not well method.

--- assign_cookies.py
+++ assign_cookies.py
@@ -11,18 +11,25 @@ def assign_cookies(g, s):
     """
     total = sum(s)
     nums = 0
-    content = 0
-    g = sorted(g, reverse=True)
-    print g
-    for i in g:
-	print i,total,nums
-	rem = total - i
+    g_idx = 0
+    s_idx = 0
+    s_len = len(s)
+    g_len = len(g)
+    g.sort(reverse=True)
+    s.sort(reverse=True)
+    print g,s
+    while True:
+	if s_idx >= s_len or g_idx >= g_len: 
+	    break
+	rem = s[s_idx] - g[g_idx]
 	if rem == 0:
 	    nums += 1
-	    break
-	elif rem > 0:
-	    total -= i
+	    g_idx += 1
+	elif rem >= 0:
 	    nums += 1
+	    g_idx += 1
 	else:
+	    g_idx += 1
 	    continue
-    return nums    
+	s_idx += 1
+    return nums
