abbfa25a8e3521b7982d77e52a3545cad2560cba
x95102003 <x95102003@gmail.com>
2017-01-03 23:54:03 +0800

    Pass first, something weird.

---
+++ delete_node_in_linkedlist.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+def deleteNode(node):
+    """TODO: Docstring for deleteNode.
+
+    :node: TODO
+    :returns: No return 
+
+    """
+    node = node.next
+
